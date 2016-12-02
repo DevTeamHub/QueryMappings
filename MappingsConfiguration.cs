4146f8d694893d0cbdcd1bd34befd03eece114c1
Dmitry Bezzubenkov <dbezzubenkov@dev-team.com>
2016-12-02 15:55:51 +0300

    Added mappings configuration

---
+++ MappingsConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace DevTeam.QueryMappings
+{
+    public static class MappingsConfiguration
+    {
+        public static void Register(params Assembly[] assemblies)
+        {
+            GetDefined(assemblies).ForEach(m => m.Setup());
+        }
+
+        private static List<IMappingsStorage> GetDefined(params Assembly[] assemblies)
+        {
+            var mappingsTypes = assemblies.SelectMany(x => x.GetTypes()).Where(t => typeof(IMappingsStorage).IsAssignableFrom(t));
+            return mappingsTypes.Select(Activator.CreateInstance).Cast<IMappingsStorage>().ToList();
+        }
+    }
+}
